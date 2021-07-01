f476be2dfd0177ae8248c0e17911b78fe719152c
kudo_hiroaki <pikoniconico26@gmail.com>
2021-07-01 20:51:21 +0900

    HTML CSS bootstrap実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,10 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
     end
-    def destroy
-      session.delete(:user_id)
-      flash[:notice] = 'ログアウトしました'
-      redirect_to new_session_path
-    end
+  end
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
