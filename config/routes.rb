01a1daacd5ebe8bbcc3ad808ca9c719d9302b967
kudo_hiroaki <pikoniconico26@gmail.com>
2021-07-01 16:06:54 +0900

    ログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+  resources :users, only: [:new, :create, :show] do
+  end
+  resources :sessions, only: [:new, :create, :destroy] do
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,10 +1,19 @@
-ActiveRecord::Schema.define(version: 2021_06_29_053353) do
+ActiveRecord::Schema.define(version: 2021_07_01_040231) do
 
   enable_extension "plpgsql"
 
   create_table "pictures", force: :cascade do |t|
     t.string "title"
     t.text "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
