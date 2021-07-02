a89330629ac5449344296c395d8399dbb9cd446d
kudo_hiroaki <pikoniconico26@gmail.com>
2021-07-02 11:01:39 +0900

    remove feeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-
-  resources :feeds
   resources :pictures do
     collection do
       post :confirm
     end
   end
-  resources :users, only: [:new, :create, :show] do
-  end
-  resources :sessions, only: [:new, :create, :destroy] do
-  end
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
